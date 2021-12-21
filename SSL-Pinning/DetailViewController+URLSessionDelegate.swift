0144252e371cf915f8a06c611d6518e442b155f8
LucianoAlmeida <passos.luciano@outlook.com>
2021-12-20 23:12:14 -0300

    [Logic] A lot of adjustments and code cleanup

--- SSL-Pinning/DetailViewController+URLSessionDelegate.swift
+++ SSL-Pinning/DetailViewController+URLSessionDelegate.swift
@@ -9,107 +9,71 @@
 import Foundation
 
 extension DetailViewController {
-    func requestWithURLSessionDelegate(pinning: Bool) {
-        guard let urlString = endpointTextfield.text,
-              let url = URL(string: urlString) else {
+  func requestWithURLSessionDelegate() {
+    guard let urlString = endpointTextfield.text,
+          let url = URL(string: urlString) else {
             showResult(success: false)
             return
-        }
-
-        /// When not pinning, we simply skip setting our own delegate		
-        let session = URLSession(configuration: .default, delegate: pinning ? self : nil, delegateQueue: nil)
-
-        let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
-            DispatchQueue.main.async { [weak self] in
-                if response != nil { self?.showResult(success: true) }
-            }
-        })
-
-        task.resume()
-    }
+          }
+    
+    /// When not pinning, we simply skip setting our own delegate
+    let session = URLSession(configuration: .default, delegate: pinning ? self : nil, delegateQueue: nil)
+    
+    let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+      DispatchQueue.main.async { [weak self] in
+        if response != nil { self?.showResult(success: true) }
+      }
+    })
+    
+    task.resume()
+  }
 }
 
 extension DetailViewController: URLSessionDelegate {
-    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        guard let trust = challenge.protectionSpace.serverTrust, SecTrustGetCertificateCount(trust) > 0 else {
-            // This case will probably get handled by ATS, but still...
-            completionHandler(.cancelAuthenticationChallenge, nil)
-            return
-        }
+  func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+    guard let trust = challenge.protectionSpace.serverTrust, SecTrustGetCertificateCount(trust) > 0 else {
+      completionHandler(.cancelAuthenticationChallenge, nil)
+      return
+    }
 
-          /// Compare the server certificate with our own stored
-//        if let serverCertificate = SecTrustGetCertificateAtIndex(trust, 0) {
-//            let serverCertificateData = SecCertificateCopyData(serverCertificate) as Data
-//
-//            if pinnedCertificates().contains(serverCertificateData) {
-//                completionHandler(.useCredential, URLCredential(trust: trust))
-//                return
-//            }
-//        }
-
-        // Or, compare the public keys
-        if let serverCertificate = SecTrustGetCertificateAtIndex(trust, 0), let serverCertificateKey = publicKey(for: serverCertificate) {
-            if pinnedKeys().contains(serverCertificateKey) {
-                completionHandler(.useCredential, URLCredential(trust: trust))
-                return
-            } else {
-                /// Failing here means that the public key of the server does not match the stored one. This can
-                /// either indicate a MITM attack, or that the backend certificate and the private key changed,
-                /// most likely due to expiration.
-                completionHandler(.cancelAuthenticationChallenge, nil)
-                showResult(success: false, pinError: true)
-                return
-            }
-        }
-
+    if let serverCertificate = SecTrustGetCertificateAtIndex(trust, 0),
+       let serverCertificateKey = publicKey(for: serverCertificate) {
+      // Compara as chave do certificado no bundle com a chave do host.
+      if let pinKey = pinnedKey, serverCertificateKey == pinKey {
+        completionHandler(.useCredential, URLCredential(trust: trust))
+        return
+      } else {
         completionHandler(.cancelAuthenticationChallenge, nil)
-        showResult(success: false)
+        showResult(success: false, pinError: true)
+        return
+      }
     }
 
-    private func pinnedCertificates() -> [Data] {
-        var certificates: [Data] = []
+    completionHandler(.cancelAuthenticationChallenge, nil)
+    showResult(success: false)
+  }
+  
+  var pinnedKey: SecKey? {
+    do {
+      let pinnedCertificateData = try Data(contentsOf: validCertURL) as CFData
+      if let pinnedCertificate = SecCertificateCreateWithData(nil, pinnedCertificateData), let key = publicKey(for: pinnedCertificate) {
+        return key
+      }
+    } catch {
+      // Handle error
+    }
+    return nil
+  }
 
-        if let pinnedCertificateURL = Bundle.main.url(forResource: "stackoverflow", withExtension: "cer") {
-            do {
-                let pinnedCertificateData = try Data(contentsOf: pinnedCertificateURL)
-                certificates.append(pinnedCertificateData)
-            } catch {
-                // Handle error
-            }
-        }
+  private func publicKey(for certificate: SecCertificate) -> SecKey? {
+    let policy = SecPolicyCreateBasicX509()
+    var trust: SecTrust?
+    let trustCreationStatus = SecTrustCreateWithCertificates(certificate, policy, &trust)
 
-        return certificates
+    guard let trust = trust, trustCreationStatus == errSecSuccess else {
+      return nil
     }
 
-    private func pinnedKeys() -> [SecKey] {
-        var publicKeys: [SecKey] = []
-
-        if let pinnedCertificateURL = Bundle.main.url(forResource: "stackoverflow", withExtension: "cer") {
-            do {
-                let pinnedCertificateData = try Data(contentsOf: pinnedCertificateURL) as CFData
-                if let pinnedCertificate = SecCertificateCreateWithData(nil, pinnedCertificateData), let key = publicKey(for: pinnedCertificate) {
-                    publicKeys.append(key)
-                }
-            } catch {
-                // Handle error
-            }
-        }
-
-        return publicKeys
-    }
-
-    /// Implementation from Alamofire
-    private func publicKey(for certificate: SecCertificate) -> SecKey? {
-        var publicKey: SecKey?
-
-        let policy = SecPolicyCreateBasicX509()
-        var trust: SecTrust?
-        let trustCreationStatus = SecTrustCreateWithCertificates(certificate, policy, &trust)
-
-        if let trust = trust, trustCreationStatus == errSecSuccess {
-            publicKey = SecTrustCopyPublicKey(trust)
-        }
-
-        return publicKey
-    }
+    return SecTrustCopyKey(trust)
+  }
 }
--- SSL-Pinning/DetailViewController.swift
+++ SSL-Pinning/DetailViewController.swift
@@ -10,50 +10,54 @@ import UIKit
 import Alamofire
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet var pinEnabledSwitch: UISwitch!
-    @IBOutlet var endpointTextfield: UITextField!
-    @IBOutlet var resultLabel: UILabel!
-
-    var method: PinMethod = .alamofire
-    var session: Session?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        /// Edit this textfield to enter another domain and simulate mismatched certificates
-        endpointTextfield.text = "https://stackoverflow.com/"
+  
+  @IBOutlet var pinEnabledSwitch: UISwitch!
+  @IBOutlet var endpointTextfield: UITextField!
+  @IBOutlet var resultLabel: UILabel!
+  
+  var method: PinMethod = .alamofire
+  var session: Session?
+  var pinning: Bool { pinEnabledSwitch.isOn }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    /// Edit this textfield to enter another domain and simulate mismatched certificates
+    endpointTextfield.text = "https://stackoverflow.com/"
+  }
+  
+  @IBAction private func testPin() {
+    switch method {
+    case .alamofire:
+      requestWithAlamofire()
+    case .NSURLSession:
+      requestWithURLSessionDelegate()
+    case .customPolicyManager:
+      requestWithCustomPolicyManager()
     }
-
-    @IBAction private func testPin() {
-        let pinning = pinEnabledSwitch.isOn
-        switch method {
-        case .alamofire:
-            requestWithAlamofire(pinning: pinning)
-        case .NSURLSession:
-            requestWithURLSessionDelegate(pinning: pinning)
-        case .customPolicyManager:
-            requestWithCustomPolicyManager(pinning: pinning)
+  }
+  
+  func showResult(success: Bool, pinError: Bool = false) {
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else { return }
+      if success {
+        self.resultLabel.text = "Success \\o/"
+      } else {
+        if pinError {
+          self.resultLabel.text = "Pinning Failed!"
+        } else {
+          self.resultLabel.text = "Request failed!"
         }
+      }
     }
+  }
+}
 
-    func showResult(success: Bool, pinError: Bool = false) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            if success {
-                self.resultLabel.textColor = UIColor(red:0.00, green:0.75, blue:0.00, alpha:1.0)
-                self.resultLabel.text = "🚀 Success"
-            } else {
-                self.resultLabel.textColor = .white
-                if pinError {
-                    self.resultLabel.text = "✅ Request failed successfully"
-                } else {
-                    self.resultLabel.text = "🚫 Request failed"
-                }
-            }
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) { [weak self] in
-                self?.resultLabel.text = ""
-            }
-        }
-    }
+extension DetailViewController {
+  var validCertURL: URL {
+    return Bundle.main.url(forResource: "stackexchange", withExtension: "cer")!
+  }
+  
+  var invalidCertURL: URL {
+    return Bundle.main.url(forResource: "stackexchange", withExtension: "cer")!
+  }
 }
